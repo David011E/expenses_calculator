11b72334ddc7be930a919d4fb00b1a2e5f3a619d
David011e <davidoliver011e@gmail.com>
2023-10-30 10:59:59 +0000

    Create get_fName and get_lName function to get users First and Last name

--- run.py
+++ run.py
@@ -12,8 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expenses_calculator')
 
-page_1 = SHEET.worksheet('page_1')
+def get_fName():
+    """
+    Get's users First name .
+    """
+    while True:
+        print("Please Enter your First Name")
+        data_str = input("Enter your First name here:\n")
+        data_str = data_str.strip()  # Remove leading and trailing spaces
 
-data = page_1.get_all_values()
+        if data_str:
+            break
+        else:
+            print("cannot leave this empty. Please try again.\n")
 
-print(data)
+get_fName()
+
+def get_lName():
+    """
+    Get's users Last name .
+    """
+    while True:
+        print("\nPlease Enter your Last Name")
+        data_str = input("Enter your Last name here:\n")
+        data_str = data_str.strip()  # Remove leading and trailing spaces
+
+        if data_str:
+            break
+        else:
+            print("cannot leave this empty. Please try again.\n")
+
+get_lName()
