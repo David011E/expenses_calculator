ccec73faf6f594f9dd43681290296a0602a9acd9
David011e <davidoliver011e@gmail.com>
2023-10-30 11:50:44 +0000

    Create get_job_position function to ask what job position they have and what they make per day

--- run.py
+++ run.py
@@ -47,3 +47,39 @@ def greet_user():
     print(f"Hello, {first_name} {last_name}! Welcome!")
 
 greet_user()
+
+def get_job_position():
+
+    job_positions = {
+        "a": {"name": "Management", "rate": 250},
+        "b": {"name": "Marketing", "rate": 320},
+        "c": {"name": "Accountant", "rate": 230},
+        "d": {"name": "Project manager", "rate": 300},
+        "e": {"name": "Business Analyst", "rate": 350},
+    }
+
+    while True:
+        print("Select your job position")
+        
+        for key, position in job_positions.items():
+            print(f"{key}: {position['name']}")
+
+        options = input("\n Select one of the above options (a-e): ").lower()
+
+        if options in job_positions:
+            while True:
+                confirm = input(
+                    f"You chose {job_positions[options]['name']}. "
+                    "Is this correct? Enter (y/n) only: ").lower()
+                if confirm == "y":
+                    selected_job = job_positions[options]
+                    print(f"You make ฿{selected_job['rate']} per day as a {selected_job['name']}.")
+                    return selected_job
+                elif confirm == "n":
+                    break  # User doesn't want to confirm, break the loop
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
+        
+        print("Please choose job position again.\n")
+
+get_job_position()
